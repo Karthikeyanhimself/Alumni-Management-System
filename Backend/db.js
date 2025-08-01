ef69cd18de3c7e7a5eff9ba7ca7dd1eafe7bbfa2
Karthikeyan Reddy <karthikeyatiyyagura73@gmail.com>
2025-08-01 09:02:21 +0530

    Create db.js

---
+++ Backend/db.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log("✅ Database Connected Successfully");
+    })
+    .catch(err => {
+        console.error("❌ Database Connection Failed:", err);
+        process.exit(1); // Exit the process on failure
+    });
+
+module.exports = mongoose;
